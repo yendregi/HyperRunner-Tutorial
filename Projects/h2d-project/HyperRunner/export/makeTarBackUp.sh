9c27cd92b41f46417e7a1787425d752af6caba6a
yendregi <zegreatjendregi@gmail.com>
2023-04-19 00:46:09 -0700

    clean up
    clean up off redundant files

--- Projects/h2d-project/HyperRunner/export/makeTarBackUp.sh
+++
@@ -1,3 +0,0 @@
-
-tar -cvf /cygdrive/c/Users/Andrzej/Desktop/Game\ Development/Java\ Game\ Examples/HyperLap2D/tutorial-space-platform-master/Projects/h2d-project/HyperRunner.tar /cygdrive/c/Users/Andrzej/Desktop/Game\ Development/Java\ Game\ Examples/HyperLap2D/tutorial-space-platform-master/Projects/h2d-project/HyperRunner
-
